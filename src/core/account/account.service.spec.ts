e2446f8b279c0da6b286eed38dd6b7b3403e6bea
Efe Hakan Gencoglu <hakan@veloxia.co>
2020-12-11 17:22:39 +0300

    Add account unit tests

---
+++ src/core/account/account.service.spec.ts
@@ -0,0 +1,90 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { AccountService } from './account.service';
+import { Account } from './entities/account.entity';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { UpdateAccountDto } from './dto/update-account.dto';
+import exp from 'constants';
+
+describe('AccountService', () => {
+  let service: AccountService;
+  let accountRepo: Repository<Account>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AccountService,
+        {
+          provide: getRepositoryToken(Account),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<AccountService>(AccountService);
+    accountRepo = module.get<Repository<Account>>(getRepositoryToken(Account));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('when creating account', () => {
+    it('should return created account', async () => {
+      const createAccountDto: CreateAccountDto = {
+        appleId: 'apple',
+        deviceId: 'device',
+        facebookId: 'facebook',
+        googleId: 'google',
+        language: 'en',
+        location: 'us',
+      };
+      const shouldReturnGroup: Account = {
+        ...createAccountDto,
+        id: '00000000-0000-0000-0000-000000000000',
+        metadata: {},
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      jest.spyOn(accountRepo, 'save').mockResolvedValueOnce(shouldReturnGroup);
+      expect(await service.create(createAccountDto)).toEqual(shouldReturnGroup);
+    });
+  });
+
+  describe('when updating account', () => {
+    it('should return updated account', async () => {
+      const updateAccountDto: UpdateAccountDto = {
+        googleId: 'alphabet',
+        location: 'uk',
+      };
+      const createAccountDto: CreateAccountDto = {
+        appleId: 'apple',
+        deviceId: 'device',
+        facebookId: 'facebook',
+        googleId: 'google',
+        language: 'en',
+        location: 'us',
+      };
+      const shouldReturnGroup: Account = {
+        ...createAccountDto,
+        id: '00000000-0000-0000-0000-000000000000',
+        metadata: {},
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      shouldReturnGroup.googleId = 'alphabet';
+      shouldReturnGroup.location = 'uk';
+      jest
+        .spyOn(accountRepo, 'findOne')
+        .mockResolvedValueOnce(shouldReturnGroup);
+      jest.spyOn(accountRepo, 'save').mockResolvedValueOnce(shouldReturnGroup);
+      expect(
+        await service.update(
+          '00000000-0000-0000-0000-000000000000',
+          updateAccountDto,
+        ),
+      ).toEqual(shouldReturnGroup);
+    });
+  });
+});
